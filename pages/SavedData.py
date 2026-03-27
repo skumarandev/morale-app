4486196265822d4a03a4f281dbcd112364eef2a6
skumarandev <kumaran.subramaniam@gmail.com>
2026-03-27 12:10:38 +0000

    Version1.3

--- pages/SavedData.py
+++ pages/SavedData.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-
+import numpy as np
 
 # 1. Check if user is already logged in
 if "authenticated" not in st.session_state:
@@ -41,14 +41,44 @@ if check_password():
     query = "SELECT * FROM responses"
     df = pd.read_sql(query, conn)
 
+
+
+    # 1. Calculate the average
+    df["average"] = df.mean(axis=1)
+
+    # 2. Define your conditions
+    conditions = [
+        (df["average"] < 7),
+        (df["average"] >= 7) & (df["average"] < 9)
+    ]
+
+    # 3. Define the corresponding values
+    choices = [1, 2]
+
+    # 4. Apply them (default handles the 'else' case)
+    df["morale"] = np.select(conditions, choices, default=3)
+
+    AveragemoraleofTeam = df["morale"].mean()
+
+    st.write("Average Morale of Team:", AveragemoraleofTeam)
+
+
     # 3. Close the connection
     conn.close()
 
     # 4. Display in Streamlit
     st.header("Responses")
-    st.dataframe(df) # Renders an interactive table
+    st.dataframe(df,hide_index=True) # Renders an interactive table
 
     if st.button("Log out"):
         st.session_state["authenticated"] = False
+        st.rerun()
+
+    if st.button("Delete survey"):
+        conn = sqlite3.connect('survey_results.db')
+        c = conn.cursor()
+        c.execute('''DELETE FROM responses''')
+        conn.commit()
+        conn.close()   
         st.rerun()
 
